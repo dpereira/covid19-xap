22f5561b8d2929d07da47beeb174edcd2b0e904c
Diego Pereira <rockin.jack@gmail.com>
2020-07-01 01:18:20 -0300

    fix: init csv data dir

--- actions.mk
+++ actions.mk
@@ -6,7 +6,7 @@ action-download-chapeco-sms: $(CHAPECO_DATA_DIR)
 	https://www.chapeco.sc.gov.br/documentos/54/documentoCategoria \
 	-P $(CHAPECO_DATA_DIR)
 
-action-pdf-scrape: scraper-setup
+action-pdf-scrape: scraper-setup $(CSV_DATA_DIR)
 	python scraper/scrape.py $(CHAPECO_DATA_DIR) $(CSV_DATA_DIR)/chapeco.csv
 
 scraper-setup:
