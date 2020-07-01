d3bc431c4bb30fe00d5687ea798dbe81b9ab0452
Diego Pereira <rockin.jack@gmail.com>
2020-07-01 01:14:07 -0300

    fix: pdf download path

--- actions.mk
+++ actions.mk
@@ -4,7 +4,7 @@ action-download-chapeco-sms: $(CHAPECO_DATA_DIR)
 	wget -c --content-disposition -nd  -r -l 1 \
 	-R 'seguranca*' -A DocumentoArquivo,pdf \
 	https://www.chapeco.sc.gov.br/documentos/54/documentoCategoria \
-	-P $(PDF_DATA_DIR)
+	-P $(CHAPECO_DATA_DIR)
 
 action-pdf-scrape: scraper-setup
 	python scraper/scrape.py $(CHAPECO_DATA_DIR) $(CSV_DATA_DIR)/chapeco.csv
