bb4c5c8be8f398b50366aa64da2dc5b35825ca8f
Diego Pereira <rockin.jack@gmail.com>
2020-07-01 00:27:34 -0300

    feat: add test workflow

---
+++ actions.mk
@@ -0,0 +1,12 @@
+include Makefile
+
+action-download-chapeco-sms: $(CHAPECO_DATA_DIR)
+	wget -c --content-disposition -nd  -r -l 1 \
+	-R 'seguranca*' -A DocumentoArquivo,pdf \
+	https://www.chapeco.sc.gov.br/documentos/54/documentoCategoria \
+	-P $(PDF_DATA_DIR)
+
+action-pdf-extract:
+	python scraper/scrape.py $(CHAPECO_DATA_DIR) $(CSV_DATA_DIR)/chapeco.csv
+
+update: action-download-chapeco-sms action-pdf-extract
