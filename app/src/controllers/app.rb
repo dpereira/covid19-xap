743f88f7d3586e468a284057c4a0ed1a2518b7a3
Diego Pereira <rockin.jack@gmail.com>
2020-08-17 23:35:35 -0300

    fix: sitemap date format

--- app/src/controllers/app.rb
+++ app/src/controllers/app.rb
@@ -417,7 +417,7 @@ class Covid19Xap < Sinatra::Application
   end
 
   get '/sitemap.xml' do
-    lastmod = self._timestamp('%y-%m-%dT%H:%M:%S')
+    lastmod = self._timestamp('%Y-%m-%d')
     return \
       "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
         <urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\"> 
